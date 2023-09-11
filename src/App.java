3be08f17639850b543ee87eb93a4befa1f4a3f1a
tran pham <trannpham.05@gmail.com>
2023-09-11 10:02:33 -0500

    added main

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number (enter exit to quit program): ");
+        String number = scan.nextLine();
+
+        while(number != "exit");
+        {
+            System.out.println("Enter a number (enter exit to quit program): ");
+            number = scan.nextLine();
+        }  
+    }
+
+    public void factor(String num)
+    {
+        int number = Integer.parseInt(num);
+
     }
 }
+
+/*
+ * 
+ */
